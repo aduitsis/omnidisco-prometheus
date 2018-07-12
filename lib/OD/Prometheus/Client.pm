46e478bb3ba2d2a7cf8aadb7553bbdb00f68c775
Athanasios Douitsis <aduitsis@cpan.org>
2018-07-12 16:28:25 +0300

    Add license

--- lib/OD/Prometheus/Client.pm
+++ lib/OD/Prometheus/Client.pm
@@ -102,6 +102,14 @@ sub get {
 	}	
 }
 
-
+=head1 COPYRIGHT & LICENSE
+ 
+Copyright 2018 Athanasios Douitsis, all rights reserved.
+ 
+This program is free software; you can use it
+under the terms of Artistic License 2.0 which can be found at 
+http://www.perlfoundation.org/artistic_license_2_0
+ 
+=cut
 
 1;
